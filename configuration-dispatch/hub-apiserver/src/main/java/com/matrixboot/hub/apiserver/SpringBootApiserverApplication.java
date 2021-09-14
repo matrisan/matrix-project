27ff28ce7e8553ee0412c5118ce8dccfc159d4bf
shishaodong <spring-boot@qq.com>
2021-09-14 14:11:12 +0800

    框架搭建

---
+++ configuration-dispatch/hub-apiserver/src/main/java/com/matrixboot/hub/apiserver/SpringBootApiserverApplication.java
@@ -0,0 +1,23 @@
+package com.matrixboot.hub.apiserver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * create in 2021/9/14 2:05 下午
+ *
+ * @author shishaodong
+ * @version 0.0.1
+ */
+
+@Slf4j
+@SpringBootApplication
+public class SpringBootApiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiserverApplication.class, args);
+    }
+
+}
---
+++ configuration-dispatch/hub-common/src/main/java/com/matrixboot/hub/common/ConfigSyncCommand.java
@@ -0,0 +1,21 @@
+package com.matrixboot.hub.common;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * create in 2021/9/14 1:52 下午
+ *
+ * @author shishaodong
+ * @version 0.0.1
+ */
+
+@Slf4j
+@Getter
+@Setter
+public class ConfigSyncCommand {
+
+
+}
