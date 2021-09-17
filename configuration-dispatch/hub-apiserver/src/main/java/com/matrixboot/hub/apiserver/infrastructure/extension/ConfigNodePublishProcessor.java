2cc9905929d166efaf08d716386827f2727f9555
shishaodong <spring-boot@qq.com>
2021-09-17 15:37:46 +0800

    配置下发 Retry

--- configuration-dispatch/hub-apiserver/src/main/java/com/matrixboot/hub/apiserver/infrastructure/extension/ConfigNodePublishProcessor.java
+++ configuration-dispatch/hub-apiserver/src/main/java/com/matrixboot/hub/apiserver/infrastructure/extension/ConfigNodePublishProcessor.java
@@ -3,6 +3,9 @@ package com.matrixboot.hub.apiserver.infrastructure.extension;
 import com.matrixboot.hub.apiserver.domain.entity.ConfigEntity;
 import com.matrixboot.hub.apiserver.domain.entity.NodeEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,18 +16,46 @@ import org.springframework.stereotype.Component;
  * @author shishaodong
  * @version 0.0.1
  */
-
+@SuppressWarnings("unused")
 @Slf4j
 @Component
 public class ConfigNodePublishProcessor implements IConfigNodeProcessor {
 
     @Override
+    @Retryable(recover = "configPreProcessorRecover", value = Exception.class, maxAttempts = 5, backoff = @Backoff(delay = 1, multiplier = 1.5))
     public void configPreProcessor(NodeEntity nodeEntity, ConfigEntity configEntity) {
         log.info("下发配置");
     }
 
     @Override
+    @Retryable(recover = "configPostProcessorRecover", value = Exception.class, maxAttempts = 5, backoff = @Backoff(delay = 1, multiplier = 1.5))
     public void configPostProcessor(NodeEntity nodeEntity, ConfigEntity configEntity) {
         log.info("删除下发的配置");
+    }
+
+
+    /**
+     * 前置处理
+     *
+     * @param nodeEntity   节点信息
+     * @param configEntity 配置信息
+     * @param e            异常信息
+     */
+    @Recover
+    public void configPreProcessorRecover(NodeEntity nodeEntity, ConfigEntity configEntity, Exception e) {
+        log.info("#health check# unhealthy: " + e.getMessage());
+
+    }
+
+    /**
+     * 后置处理
+     *
+     * @param nodeEntity   节点信息
+     * @param configEntity 配置信息
+     * @param e            异常信息
+     */
+    @Recover
+    public void configPostProcessorRecover(NodeEntity nodeEntity, ConfigEntity configEntity, Exception e) {
+        log.info("#health check# unhealthy: " + e.getMessage());
     }
 }
